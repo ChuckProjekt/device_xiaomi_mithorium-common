89d04be95926f0a8115e23e6b33ce55805d4698a
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    mi-common: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf

--- mithorium.mk
+++ mithorium.mk
@@ -419,6 +419,10 @@ MITHORIUM_PRODUCT_PACKAGES += \
     init.qti.dcvs.sh
 endif
 
+# Remove Packages
+MITHORIUM_PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 MITHORIUM_PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
